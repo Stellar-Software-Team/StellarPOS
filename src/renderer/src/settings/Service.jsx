a3aa87debd14d32c5a7e6962ebc5effd77ff6154
Josue <josue.medina9991@gmail.com>
2024-09-15 11:56:56 -0600

    First Look ended Settings

--- src/renderer/src/settings/Service.jsx
+++ src/renderer/src/settings/Service.jsx
@@ -23,7 +23,8 @@ function Service() {
 
           <div className="grid grid-cols-2 gap-6 mt-8">
             <div className="grid gap-2">
-                <h1 className="font-bold text-lg">General Set up</h1>
+               <div>
+               <h1 className="font-bold text-lg">General Set up</h1>
                 <Checkbox
                 label={
                   <div>
@@ -39,66 +40,78 @@ function Service() {
                   className: '-mt-5'
                 }}
               />
-             <div className='flex space-x-16 items-center'>
+               </div>
+             
+             <h1 className='font-bold text-lg'>General cash register</h1>
+            <div className='flex space-x-16 items-center'>
               <div>
-              <h1 className='font-semibold text-md'>Open General cash register at:</h1>
+              <h1 className='text-gray-800 text-md'>Open General cash register at:</h1>
                 <input
                     type="time"
                     className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
               <div>
-              <h1 className='font-semibold text-md'>Close General cash register at:</h1>
+              <h1 className='text-gray-800 text-md'>Close General cash register at:</h1>
                 <input
                     type="time"
                     className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
              </div>
-
-             
-            </div>
-
-            
-
-            <div className="flex flex-col gap-2">
-              <h1 className="font-bold text-lg">Doble system</h1>
-              <Checkbox
-                label={
-                  <div>
-                    <Typography color="blue-gray" className="font-medium">
-                      Active doble work schedule
-                    </Typography>
-                    <Typography variant="small" color="gray" className="font-normal">
-                      This will set the work schedule in the morning and evening
-                    </Typography>
-                  </div>
-                }
-                containerProps={{
-                  className: '-mt-5'
-                }}
-              />
-
-                      
-
-
-            </div>
-            <div className="grid gap-2">
-             
             </div>
 
             <div className="grid gap-2">
+            <div>
+            <h1 className="font-bold text-lg">Cash reconciliation</h1>
+                <Checkbox
+                  label={
+                    <div>
+                      <Typography color="blue-gray" className="font-medium">
+                        Active cash reconciliation
+                      </Typography>
+                      <Typography variant="small" color="gray" className="font-normal">
+                        Option to reconcile cash between cash register and the sales total
+                      </Typography>
+                    </div>
+                  }
+                  containerProps={{
+                    className: '-mt-5'
+                  }}
+                />
+            </div>
+            </div>
+
+            <div className="grid gap-2">
+           <div>
+            <h1 className="font-bold text-lg">Doble system</h1>
+                <Checkbox
+                  label={
+                    <div>
+                      <Typography color="blue-gray" className="font-medium">
+                        Active doble work schedule
+                      </Typography>
+                      <Typography variant="small" color="gray" className="font-normal">
+                        This will set the work schedule in the morning and evening
+                      </Typography>
+                    </div>
+                  }
+                  containerProps={{
+                    className: '-mt-5'
+                  }}
+                />
+            </div>
             <div className='flex justify-left mt-2'><h1 className='font-bold text-lg'>Morning work schedule</h1></div>
                     <div className='flex space-x-16 items-center'>
                       <div className=''>
-                      <h1 className='font-semibold text-md'>Open General cash register at:</h1>
+                      <h1 className='text-gray-800 text-md'>Open General cash register at:</h1>
                         <input
                             type="time"
                             className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                       </div>
                       <div>
-                      <h1 className='font-semibold text-md'>Close General cash register at:</h1>
+                      <h1 className='text-gray-800 text-md'>Close General cash register at:</h1>
                         <input
                             type="time"
                             className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -109,22 +122,50 @@ function Service() {
                     <div className='flex justify-left mt-2'><h1 className='font-bold text-lg'>Evening work schedule</h1></div>
                     <div className='flex space-x-16 items-center'>
                       <div className=''>
-                      <h1 className='font-semibold text-md'>Open General cash register at:</h1>
+                      <h1 className='text-gray-800 text-md'>Open General cash register at:</h1>
                         <input
                             type="time"
                             className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                       </div>
                       <div>
-                      <h1 className='font-semibold text-md'>Close General cash register at:</h1>
+                      <h1 className='text-gray-800 text-md'>Close General cash register at:</h1>
                         <input
                             type="time"
                             className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                       </div>
                     </div>
+            <div>
+              
+            </div>
+           </div>
+
+            <div className="grid gap-2">
+            <div>
+            <h1 className="font-bold text-lg">Cash register resume</h1>
+                <Checkbox
+                defaultChecked
+                  label={
+                    <div>
+                      <Typography color="blue-gray" className="font-medium">
+                      Automaticly create a resume after closing a cash register
+                      </Typography>
+                      <Typography variant="small" color="gray" className="font-normal">
+                        Option to create a resume after closing a cash register with the totalof sales, cash earned, etc.
+                      </Typography>
+                    </div>
+                  }
+                  containerProps={{
+                    className: '-mt-5'
+                  }}
+                />
+            </div>
             </div>
             
+            <div className='grid gap-2'>
+           
+            </div>
           </div>
           <div className="flex justify-start mt-4">
             <Button size="sm">Save</Button>{' '}
@@ -133,66 +174,20 @@ function Service() {
 
         <div className="bg-white rounded-2xl shadow-md p-6">
           <h1 className="font-bold text-xl">Service Register</h1>
-          <span className="text-gray-500">Update the view of the application</span>
+          <span className="text-gray-500">Set a Service Register Configuration</span>  
 
           <div className="grid grid-cols-2 gap-6 mt-8">
-            <div className="flex  items-end gap-2">
-              <div className="grid gap-2">
-                <h1 className="font-bold text-lg">Theme Mode</h1>
-                <Select
-                  label=""
-                  //           value={value}
-                  // onChange={(val) => setValue(val)}
-                  variant="outlined"
-                  className="text-black  !border-black"
-                >
-                  <Option className="text-black">White Theme</Option>
-                  <Option className="text-black">Dark Theme</Option>
-                </Select>
-              </div>
-              <div className="grid gap-2 ">
-                <h1 className="font-bold text-lg">Palette</h1>
-                <Select
-                  label=""
-                  //           value={value}
-                  // onChange={(val) => setValue(val)}
-                  variant="outlined"
-                  className="text-black border-black"
-                >
-                  <Option className="text-black">Coffee</Option>
-                  <Option className="text-black">Moderm UI</Option>
-                </Select>
-              </div>
-            </div>
-
             <div className="grid gap-2">
-              <h1 className="font-bold text-lg">Animations</h1>
-              <Checkbox
+               <div>
+               <h1 className="font-bold text-lg">General Set up</h1>
+                <Checkbox
                 label={
                   <div>
                     <Typography color="blue-gray" className="font-medium">
-                      Activate
+                      Automaticly open General service register at certain time 
                     </Typography>
                     <Typography variant="small" color="gray" className="font-normal">
-                      Animations will be displayed by default
-                    </Typography>
-                  </div>
-                }
-                containerProps={{
-                  className: '-mt-5'
-                }}
-              />{' '}
-            </div>
-            <div className="grid gap-2">
-              <h1 className="font-bold text-lg">Notifications</h1>
-              <Checkbox
-                label={
-                  <div>
-                    <Typography color="blue-gray" className="font-medium">
-                      Activate
-                    </Typography>
-                    <Typography variant="small" color="gray" className="font-normal">
-                      Notifications will be displayed by default
+                      it's needed to set a time for automaticly open the service register. Default is 8:00 AM to 8:00 PM
                     </Typography>
                   </div>
                 }
@@ -200,6 +195,131 @@ function Service() {
                   className: '-mt-5'
                 }}
               />
+               </div>
+             
+             <h1 className='font-bold text-lg'>General service register</h1>
+            <div className='flex space-x-16 items-center'>
+              <div>
+              <h1 className='text-gray-800 text-md'>Open General service register at:</h1>
+                <input
+                    type="time"
+                    className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+              <h1 className='text-gray-800 text-md'>Close General service register at:</h1>
+                <input
+                    type="time"
+                    className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+             </div>
+            </div>
+
+            <div className="grid gap-2">
+            <div>
+            <h1 className="font-bold text-lg">Cash reconciliation</h1>
+                <Checkbox
+                  label={
+                    <div>
+                      <Typography color="blue-gray" className="font-medium">
+                        Active cash reconciliation
+                      </Typography>
+                      <Typography variant="small" color="gray" className="font-normal">
+                        Option to reconcile cash between cash register and the sales total
+                      </Typography>
+                    </div>
+                  }
+                  containerProps={{
+                    className: '-mt-5'
+                  }}
+                />
+            </div>
+            </div>
+
+            <div className="grid gap-2">
+           <div>
+            <h1 className="font-bold text-lg">Doble system</h1>
+                <Checkbox
+                  label={
+                    <div>
+                      <Typography color="blue-gray" className="font-medium">
+                        Active doble work schedule
+                      </Typography>
+                      <Typography variant="small" color="gray" className="font-normal">
+                        This will set the work schedule in the morning and evening
+                      </Typography>
+                    </div>
+                  }
+                  containerProps={{
+                    className: '-mt-5'
+                  }}
+                />
+            </div>
+            <div className='flex justify-left mt-2'><h1 className='font-bold text-lg'>Morning work schedule</h1></div>
+                    <div className='flex space-x-16 items-center'>
+                      <div className=''>
+                      <h1 className='text-gray-800 text-md'>Open General cash register at:</h1>
+                        <input
+                            type="time"
+                            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                      </div>
+                      <div>
+                      <h1 className='text-gray-800 text-md'>Close General cash register at:</h1>
+                        <input
+                            type="time"
+                            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                      </div>
+                    </div>
+
+                    <div className='flex justify-left mt-2'><h1 className='font-bold text-lg'>Evening work schedule</h1></div>
+                    <div className='flex space-x-16 items-center'>
+                      <div className=''>
+                      <h1 className='text-gray-800 text-md'>Open General cash register at:</h1>
+                        <input
+                            type="time"
+                            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                      </div>
+                      <div>
+                      <h1 className='text-gray-800 text-md'>Close General cash register at:</h1>
+                        <input
+                            type="time"
+                            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                      </div>
+                    </div>
+            <div>
+              
+            </div>
+           </div>
+
+            <div className="grid gap-2">
+            <div>
+            <h1 className="font-bold text-lg">Service register resume</h1>
+                <Checkbox
+                defaultChecked
+                  label={
+                    <div>
+                      <Typography color="blue-gray" className="font-medium">
+                      Automaticly create a resume after closing a cash register
+                      </Typography>
+                      <Typography variant="small" color="gray" className="font-normal">
+                        Option to create a resume after closing a cash register with the totalof sales, cash earned, etc.
+                      </Typography>
+                    </div>
+                  }
+                  containerProps={{
+                    className: '-mt-5'
+                  }}
+                />
+            </div>
+            </div>
+            
+            <div className='grid gap-2'>
+           
             </div>
           </div>
           <div className="flex justify-start mt-4">
