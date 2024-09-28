11d68710d4b1acabbf892933def318628e4cc0d0
Josue <josue.medina9991@gmail.com>
2024-09-28 11:00:53 -0600

    fixing issues

--- src/renderer/src/service/GeneralService.jsx
+++ src/renderer/src/service/GeneralService.jsx
@@ -330,7 +330,6 @@ function ItemPanel(product) {
         }
     })
 
-    console.log(sizes)
   return (
     <div className='w-96 h-[26rem] bg-white shadow-xl rounded-3xl flex flex-col p-2'>
         <section className='flex p-4 gap-2 basis-[40%]'>
@@ -421,18 +420,21 @@ function ItemPanel(product) {
                 </div>
                 )
             }
-            <div className='flex flex-row gap-2 h-full items-center basis-[10%] mx-2'>
-            <div className='basis-[10%]'>
+            <div className='flex flex-row gap-2 h-full items-center justify-end basis-[10%] mx-2 mb-2'>
+            {/* <div className='basis-[10%]'>
                 <Button>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
+                </Button> 
+            </div> */}
+            <div className='basis-[20%]'>
+                <Button fullWidth  className='flex flex-row gap-2 justify-center items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
                 </Button>
-            </div>
-            <div className='basis-[90%] '><Button fullWidth  className='flex flex-row gap-2 justify-center items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-</svg>
- Add to cart</Button></div>
+                </div>
             </div>
     </div>
   )
